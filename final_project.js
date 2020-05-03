c64162a7894180d6284830e2c6eb2f1ee725bf49
Henry Gunner <henrygunner@Henrys-MBP.home>
2020-05-03 16:47:50 -0400

    Submission for Tuesday

--- final_project.js
+++ final_project.js
@@ -18,7 +18,7 @@ var drawPie = function(datas,width,height)
     var w = graph.width;
     var h = graph.height;
     var outer_r = h/2;
-    var inner_r = 0;
+    var inner_r = h/4;
     
     var arc = d3.arc()
         .innerRadius(inner_r)
@@ -29,32 +29,75 @@ var drawPie = function(datas,width,height)
             .attr("width",graph.width)
             .attr("height",graph.height);
     
-    test_data = [5,10,15,20,60];
-    console.log("datas: "+datas);
-    /*values = d3.map(datas,function(data){return data.value;});
-    console.log("values: "+values);*/
-    var arcs = target.selectAll("g.arc") //binds data to the wedges. generate pie-effied data
-        .data(pie(test_data))
+    values = datas.map(function(data){return data.value;});
+    console.log("values:");
+    console.log(values);
+    var arcs = target.selectAll(".arc") //binds data to the wedges. generate pie-effied data
+        .data(pie(values))
         .enter()
         .append("g")
-        .attr("class","arc")
-        .attr("transform","translate("+2*outer_r+","+graph.height/2+")");
+        .classed("arc","true")
+        .attr("transform","translate("+2*outer_r+","+graph.height/2+")")
+    
+        .on("mouseover",function(value){
+            d3.selectAll(".arc").classed("fade",true);
+            d3.select(this).classed("fade",false);
+            d3.select(this).select("#sector")
+                .classed("hidden",false);
+            
+        })
+        .on("mouseout",function(value){
+            d3.selectAll(".arc").classed("fade",false);
+            d3.selectAll(".arc").select("#sector")
+                .classed("hidden",true);
+        });
     
     arcs.append("path")
         .attr("fill",function(data,i){
               return color(i);
         })
         .attr("d",arc);
+    
+    arcs.append("text")
+        .attr("id","sector")
+        .attr("transform",function(data){
+            c = arc.centroid(data);
+            x = c[0];
+            y = c[1]-40; //+20;
+            return "translate("+x+","+y+")";
+        })
+        .attr("text-anchor","middle")
+        .text(function(data,i){
+            return datas[i].sector; 
+        })
+        .classed("hidden","false");
+    
+     arcs.append("text")
+        .attr("transform",function(data){
+            c = arc.centroid(data);
+            x = c[0];
+            y = c[1]; //+20;
+            return "translate("+x+","+y+")";
+        })
+       .attr("id","value")
+       .attr("text-anchor","middle")
+       .text(function(data){
+            return data.value + "%"; 
+        });
+        
+    /*target.append("text")
+        .text("percent of C02 emissions")
+        .attr("transform","translate("+2*h/3+","+h/2+")");*/
 }
 
-//var emissionsPromise = d3.json("https://blackfyrehg.github.io/Final_Project/emissions_by_sector.json");
-var emissionsPromise = d3.json("emissions_by_sector.json");
+
+var emissionsPromise = d3.json("https://blackfyrehg.github.io/Final_Project/emissions_by_sector.json");
+//var emissionsPromise = d3.json("emissions_by_sector.json");
 emissionsPromise.then(function(emissions) {
     var width = 1600;
     var height= 500;
     console.log("window has height: "+height+" , and width:"+width);
-   // console.log("emissions: "+emissions[0].value)
-    drawPie(emissions,width,height);
+    drawPie(emissions ,width,height);
     
 }, function(err) {
     console.log("failed to get student data:", err);
