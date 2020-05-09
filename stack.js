ef4d8dd386ca6c69f8d3fb0982dd164263f97828
Henry Gunner <henrygunner@Henrys-MacBook-Pro.local>
2020-05-09 14:54:38 -0400

    template for new visualization

---
+++ stack.js
@@ -0,0 +1,279 @@
+var getString = function(data)
+{
+    //console.log(data);
+    if(data[1]==data.data.g) //checks if the top of the rectangle is the same as the bottom. 
+    {
+        string = "grape";
+    }
+    else if(data[0]==data.data.g)
+    {
+        string = "orange";
+    }
+    else 
+    {
+        string = "apple"
+    }
+    return string; 
+}
+
+var drawStack = function(datas,width,height)
+{
+    var screen = {width:width, height:height};
+    var margins = {top:30, bottom:30, left:70, right:40};
+    var graph = 
+    {
+        width: screen.width - margins.left - margins.right,
+        height: screen.height - margins.top - margins.bottom,
+    }
+    var xTitle = "Xtitle is constant";
+    var yTitle = "this is the initial Y";
+    
+    var svg = d3.select("body") //svg object drawn too
+        .append("svg")
+            .attr("width",width)
+            .attr("height",height);
+    
+    //--------------Creates scales and axis--------------
+    var xScale = d3.scaleBand()
+                .domain(d3.range(dataset.length))
+                .range([margins.left, graph.width])
+                .paddingInner(0.05);
+    
+    var yScale = d3.scaleLinear()
+                .domain([0,85])
+                .range([graph.height, margins.bottom]);
+    
+    createAxes(margins,graph,svg,xScale,yScale);
+    changeXticks(margins,graph,svg,xScale);
+    //---------------------------------------------------
+    
+    //gives axis labels. 
+    setAxesTitles(margins, graph, "svg", xTitle, yTitle)
+    
+    var stack = d3.stack()
+                  .keys(["a","o","g"])
+                  .order(d3.stackOrderDescending); //puts order as largest to smallest.
+    
+    var series = stack(datas);
+    var colors = d3.scaleOrdinal(d3.schemeDark2);
+    
+    //Add a group for each row of data
+    var groups = svg.selectAll(".stack")
+                .data(series)
+                .enter()
+                .append("g")
+                .attr("class","stack")
+                .style("fill",function(d,i){
+                    return colors(i);
+                });/*
+                .on("mouseover",function(column){
+                    d3.selectAll(".stack").classed("fade",true);
+                    d3.select(this).classed("fade",false);
+
+                })
+                .on("mouseout",function(value){
+                    d3.selectAll(".stack").classed("fade",false);
+                });*/
+    
+    //Add a rectangle for each data value
+    var rects = groups.selectAll("rect")
+                .data(function(data){return data;}) //grabs child elements of "series" and binds those to the rectangles. 
+                .enter()
+                .append("rect")
+                .attr("x",function(d,i){
+                    return xScale(i);
+                })
+                .attr("y",function(d){
+                    return yScale(d[1]);
+                })
+                .attr("height",function(d){
+                    return yScale(d[0])-yScale(d[1]);
+                })
+                .attr("width",xScale.bandwidth())
+                .attr("id",function(d,i){return "column_"+i;});
+    
+    //---------------Changing rect labels and Axis titles----------------
+    rects.on("mouseover",function(column){
+        var column_id = "#"+this.id;
+        var label_id = "#label_"+this.id
+        var newTitle = findTitle(column_id);
+        var new_yScale = getNewYscale(graph,margins,series,column_id);
+        updateAxes("svg",xScale,new_yScale);
+        setAxesTitles(margins, graph, "svg", xTitle, newTitle);
+        d3.selectAll("rect").classed("fade",true);
+        d3.selectAll(column_id).classed("fade",false);
+        d3.selectAll(label_id).classed("hidden",false);
+    })
+    .on("mouseout",function(value){
+        var label_id = "#label_"+this.id;
+        updateAxes("svg",xScale,yScale);
+        setAxesTitles(margins, graph, "svg", xTitle, yTitle);
+        d3.selectAll("rect").classed("fade",false);
+        d3.selectAll(label_id).classed("hidden",true);
+    });
+    //----------------------------------------------------------------------
+    
+    //Makes text labels for each rect in the stack
+    groups.selectAll("text")
+                .data(function(data){return data;})
+                .enter()
+                .append("text")
+                .attr("text-anchor", "middle")
+                .attr("x", function(rect, index)
+                {
+                    return xScale(index)+margins.left-10;
+                })
+                .attr("y", function(data)
+                {
+                    return yScale(data[0]) - (yScale(data[0])-yScale(data[1]))/2;  
+                })
+                .attr("fill","black")
+                .attr("id", function(d,i){return "label_column_"+i;})
+                .classed("rect_label",true)
+                .text(function(data){return getString(data);})
+                .classed("hidden",true);
+}
+
+var createAxes = function(margins,graph,target,xScale,yScale)
+{
+    // Setup axes
+    var xAxis = d3.axisBottom(xScale)
+    var yAxis = d3.axisLeft(yScale);
+    
+    // Draw the axes
+    target.append("g")
+        .attr("class","xaxis")
+        .attr("transform","translate("+0+","+(graph.height)+")")
+        .call(xAxis);
+     target.append("g")
+        .attr("class","yaxis")
+        .attr("transform","translate("+margins.left+","+0+")")
+        .call(yAxis);
+    
+}
+
+//Creates the xlabels for each column on the graph. 
+var changeXticks = function(margins,graph,target,xScale)
+{
+    tick_labels = ["group1","group2","gorup3","group4","group5","group6"];
+    d3.select(".xaxis").selectAll(".tick").select("text").text('');
+    var xticks = target.append("g").classed("xticks",true);
+    for (var i = 1; i <=tick_labels.length;i++)
+    {
+        console.log(margins.left);
+        var x = i*(xScale.bandwidth()+4) + margins.left - xScale.bandwidth()/2;
+        var y = graph.height + margins.bottom/1.5;
+        xticks.append("text")
+        .attr("text-anchor", "middle")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("fill","black")
+                .text(tick_labels[i-1]);
+        //.attr("transform","rotate(45)");
+    }
+}
+
+// Sets the titles of the axes
+var setAxesTitles = function(margins, graph, target, xTitle, yTitle) 
+{
+    var labels = d3.select(target)
+                   .append("g")
+                   .classed("labels", true);
+    labels.append("text")
+        .text(xTitle)
+        .classed("label", true)
+        .attr("text-anchor", "middle")
+        .attr("x", margins.left + (graph.width / 2))
+        .attr("y", graph.height + margins.bottom+20);
+    labels.append("g")
+        .attr("transform","translate(20," + 
+              (margins.top + (graph.height / 2)) + ")")
+        .append("text")
+        .text(yTitle)
+        .classed("label", true)
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+}
+
+var updateAxes = function(target, xScale, yScale) 
+{
+    console.log("updated");
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+    
+    /*d3.select(".x-axis")
+        .transition()
+        .duration(1000)
+        .call(xAxis)*/
+    
+    d3.select(".yaxis")
+        .transition()
+        .duration(1000)
+        .call(yAxis)
+    
+    d3.select(target)
+        .selectAll(".labels")
+        .remove();
+}
+
+var findTitle = function(id)
+{
+    var title= "Error";
+    if(id=="#column_0"){ //checks if the top of the rectangle is the same as the bottom. 
+        title = "zeroes";
+    }
+    else if(id=="#column_1"){title = "firsts";}
+    else if(id=="#column_2"){title = "seconds";}
+    else if(id=="#column_3"){title = "thirds";}
+    else if(id=="#column_4"){title = "fourths";}
+    else {title = "fifths";}
+    return title; 
+}
+
+var  getNewYscale = function(graph,margins,series,id)
+{
+    var yScale = d3.scaleLinear()
+            .domain([0, 80])
+            .range([graph.height, margins.top]);
+    if(id=="#column_0"){ //checks if the top of the rectangle is the same as the bottom. 
+        yScale = d3.scaleLinear()
+            .domain([0, 110])
+            .range([graph.height, margins.top]);
+    }
+    else if(id=="#column_1"){
+        yScale = d3.scaleLinear()
+            .domain([0, 120])
+            .range([graph.height, margins.top]);
+    }
+    else if(id=="#column_2"){
+        yScale = d3.scaleLinear()
+            .domain([0, 130])
+            .range([graph.height, margins.top]);
+    }
+    else if(id=="#column_3"){
+        yScale = d3.scaleLinear()
+            .domain([0, 140])
+            .range([graph.height, margins.top]);
+    }
+    else if(id=="#column_4"){
+        yScale = d3.scaleLinear()
+            .domain([0, 150])
+            .range([graph.height, margins.top]);
+    }
+    else {
+        yScale = d3.scaleLinear()
+            .domain([0, 160])
+            .range([graph.height, margins.top]);
+    }
+    return yScale;
+}
+
+
+var emissionsPromise = d3.json("stack_data.json");
+//var emissionsPromise = d3.json("emissions_by_sector.json");
+emissionsPromise.then(function(emissions) {
+    drawStack(dataset,800,500);
+    
+}, function(err) {
+    console.log("failed to get student data:", err);
+});
